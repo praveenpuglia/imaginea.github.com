c52cf5c36e8c8c7274df998a0590b2039433cee7
varunimaginea <varunp@varun-p.(none)>
2012-07-18 18:23:01 +0530

    Fireflow and Pancake-IOS download count updated.

--- js/projectLoader.js
+++ js/projectLoader.js
@@ -66,8 +66,8 @@ var LoadDetails = (function()
 			{
 				case 0: 
 					var downloadStats = {
-							"FireFlow": { "download_count": 9118, "download_url": "https://addons.mozilla.org/en-US/firefox/addon/fireflow/statistics/?last=30"	},
-							"pancake-ios": { "download_count": 160, "download_url": "http://itunes.apple.com/us/app/sugar-on-pancake/id528250369?mt=8"}
+							"FireFlow": { "download_count": 9364, "download_url": "https://addons.mozilla.org/en-US/firefox/addon/fireflow/statistics/?last=30"	},
+							"pancake-ios": { "download_count": 290, "download_url": "http://itunes.apple.com/us/app/sugar-on-pancake/id528250369?mt=8"}
 						};
 					if(downloadStats[projectName])
 					{
